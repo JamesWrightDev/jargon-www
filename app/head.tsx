7b694344c5abea777a8e8260970bc3ceb9b0bca7
James Wright <32226552+JamesWrightDev@users.noreply.github.com>
2022-12-11 20:55:06 +0000

    add freshdesk

--- app/head.tsx
+++ app/head.tsx
@@ -1,3 +1,5 @@
+import Script from "next/script";
+
 export default function Head() {
   return (
     <>
@@ -5,6 +7,21 @@ export default function Head() {
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Jargon App" />
       <link rel="icon" href="/favicon.ico" />
+
+      <Script>
+        {`
+    window.fwSettings={
+      'widget_id':103000003003,
+      'locale': 'en'
+    };
+    !function(){if("function"!=typeof window.FreshworksWidget){var n=function(){n.q.push(arguments)};n.q=     [],window.FreshworksWidget=n}}()`}
+      </Script>
+      <Script
+        type="text/javascript"
+        src="https://widget.freshworks.com/widgets/103000003003.js"
+        async
+        defer
+      />
     </>
   );
 }
